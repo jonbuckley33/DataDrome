fd04d2ed131fe7d5a7c832c0f4984959ef71d858
Jon Buckley <jonbuckley33@gmail.com>
2013-06-05 19:25:35 -0400

    added javascript file

---
+++ parse_data_file.js
@@ -0,0 +1,14 @@
+class DataDump
+{
+	//should contain array of Data objects
+}
+
+class DataPoint
+{
+	//should contain variables for location, data value, etc.
+}
+
+function parse(file)
+{
+	//should take a File as a parameter and return a DataDump object
+}
